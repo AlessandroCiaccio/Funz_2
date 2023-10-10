a1a3718dcb89cfdac5aab8d0b3e696a498f4cfb5
Alessandro Scrofani <sandroscro@libero.it>
2023-10-10 19:21:40 +0200

    Added Main

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String [] arg){
+        String str="Francesco";
+        System.out.print("The string contains " +Length(str)+ " characters");
+    }
+    public static int Length(String x){
+        return (x.length());
+    }
+}
